8ae1aa49d4c365f72901266456ffd1630dde7cde
niath63 <156790201+niath63@users.noreply.github.com>
2024-01-16 16:28:10 +0530

    Add files via upload

---
+++ Conditional ..py
@@ -0,0 +1,35 @@
+
+
+
+
+user_name=input("Ender your username")
+password=input("Enter your password")
+
+
+A="Addbook"
+B="DeleteBook"
+C="ViewBook"
+D="ReturnBook"
+E="COBook"
+F="UBBook"
+E="EXIT"
+
+option=input("Select any option [A/B/C/D/E/F]")
+if(option=="A"):
+    print(A)
+    Bookname=input("Bookname")
+elif(option=="B"):
+    print(B)
+    Delet=input("Booknumber")
+elif(option=="C"):
+    print(C)
+elif(option=="D"):
+    print(D)
+elif(option=="E"):
+    print(E)
+elif(option=="F"):
+    print(F)
+elif(option=="E"):
+    print(E)
+
+

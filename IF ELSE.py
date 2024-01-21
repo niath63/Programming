893cab6ba0e3bc7be6be3276359df94eb5e22f5c
niath63 <156790201+niath63@users.noreply.github.com>
2024-01-21 19:14:13 +0530

    Add files via upload

---
+++ IF ELSE.py
@@ -0,0 +1,26 @@
+'''i=20
+if (i < 15):
+    print("True")
+
+else:
+    print("False")'''
+
+
+
+'''i =float(input("Enter any number"))
+if (i>0):
+    print("Positive")
+elif (i<0):
+    print("Negative")
+else:
+    print("Zero")'''
+i=float(input("Ender Number"))
+if(i>=0):
+    if(i>0):
+        print("Number is Possitive")
+    else:
+        print("Number is Zero")
+else:
+    print("Number is Neegative")
+
+

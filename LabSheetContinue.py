74e97b2ae6f4d3f6f2b2e4be25d7dd2c212f5f2c
niath63 <156790201+niath63@users.noreply.github.com>
2024-01-23 15:58:34 +0530

    Add files via upload

---
+++ LabSheetContinue.py
@@ -0,0 +1,114 @@
+# 4t
+'''a=str(input("Enter cher:"))
+if a.isalpha():
+    print(a,"is Alphabet")
+else:
+    print(a,"is not Alphabet")'''
+# 6th task
+'''a=float(input("Enter number"))
+if(a<0):
+    print(a,"is negative")
+else:
+    print("invalid number")'''
+# 7th task
+'''x=int(input("Enter Number"))
+if(x%2==0):
+    print(x,"is odd number")
+else:
+    print(x,"is even number")'''
+# 8th task
+'''a=float(input("Enter no a: "))
+b=float(input("Enter no b: "))
+if(a==b):
+    print(a,"is equal",b )
+elif(a>b):
+    print(a,"is greater then",b)
+else:
+    print(a,"is less then",b)'''
+# 9th task
+'''no= float(input("Enter a number: "))
+if(no>0):
+   print(no,"is a positive")
+elif (no< 0):
+    print(no,"is a negative")
+else:
+ print(no,"is zero")'''
+# 10th task
+'''marks=float(input("Enter Marks"))
+if(marks>=90):
+    print(marks,"marks in Grade A")
+elif(marks>=80):
+    print(marks, "marks in Grade B")
+elif(marks>=70):
+    print(marks, "marks in Grade C")
+else:
+    print(marks, "marks in Grade D")'''
+# 11th task
+
+'''a=int(input("Ender Number"))
+if(a ==1):
+    print("Monday")
+elif(a==2):
+    print("Tuesday")
+elif(a==3):
+    print("Wednesday")
+elif(a==4):
+    print("Thursday")
+elif(a ==5):
+    print("Friday")
+elif(a ==6):
+    print("Suterday")
+elif(a ==7):
+    print("Sunday")
+else:
+    print("Invelid day")'''
+# 12th task
+'''CP=float(input("Enter CP"))
+SP=float(input("Enter SP"))
+
+if(CP>0 and SP>0):
+    if((SP-CP)>0):
+        print("Gain")
+    elif((SP-CP)<0):
+        print("Loss")
+    elif((SP-CP)==0):
+        print("Nothing")'''
+# 13th task
+'''a=int(input("Enter 2digit No"))
+b=int(input("Enter No"))
+
+if(a%b==0):
+    print("Yes")
+else:
+    print("No")'''
+# 14th
+'''speed=float(input("Enter speed"))
+if(31<=speed<=40):
+    print("50 pounds")
+elif(41<=speed<=50):
+    print("75 pounds")
+elif(speed>50):
+    print("100 pounds")
+else:
+    print("No Fine")'''
+# 15th
+a=float(input("Enter Amount"))
+Dis=15
+Dis2=10
+C=Dis*a/100
+D=Dis2*a/100
+
+if(a>0):
+    if(a>=5000):
+        print(C)
+    elif(a<5000):
+        print(D)
+
+
+
+
+
+
+
+
+
